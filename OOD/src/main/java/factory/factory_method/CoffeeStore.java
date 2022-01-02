693e125763633be56a679d9a24a8b593fb2b9f1b
ylyu <yanan.lyu@gmail.com>
2022-01-02 15:58:05 -0800

    factory method

--- OOD/src/main/java/factory/factory_method/CoffeeStore.java
+++ OOD/src/main/java/factory/factory_method/CoffeeStore.java
@@ -1,18 +1,20 @@
 package factory.factory_method;
 
-import factory.simplefactory.coffee.Coffee;
-import factory.simplefactory.coffee.SimpleCoffeeFactory;
-
 /**
  * @author ylyu
  * @date 1/2/22 9:06 AM
  * @version 1.0
  */
 public class CoffeeStore {
-    public factory.simplefactory.coffee.Coffee orderCoffee(String type) {
-        SimpleCoffeeFactory factory = new SimpleCoffeeFactory();
-        Coffee coffee = factory.createCoffee(type);
 
+    private CoffeeFactory factory;
+
+    public void setFactory(CoffeeFactory factory) {
+        this.factory = factory;
+    }
+
+    public Coffee orderCoffee() {
+        Coffee coffee = factory.createCoffee();
         coffee.addMilk();
         coffee.addSugar();
 
