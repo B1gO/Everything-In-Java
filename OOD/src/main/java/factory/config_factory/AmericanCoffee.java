e494c6efdbe198c2f29aec2ad75b1c9a01e5e995
ylyu <yanan.lyu@gmail.com>
2022-01-03 07:55:46 -0800

    config factory

---
+++ OOD/src/main/java/factory/config_factory/AmericanCoffee.java
@@ -0,0 +1,13 @@
+package factory.config_factory;
+
+/**
+ * @author ylyu
+ * @date 1/2/22 9:04 AM
+ * @version 1.0
+ */
+public class AmericanCoffee extends Coffee {
+    @Override
+    public String getName() {
+        return "American Coffee";
+    }
+}
