d3dd6f95fe58f5876d3485fb6b576199bb00c609
ylyu <yanan.lyu@gmail.com>
2022-01-02 16:24:48 -0800

    tract factory

---
+++ OOD/src/main/java/factory/abstract_factory/DessertFactory.java
@@ -0,0 +1,11 @@
+package factory.abstract_factory;
+
+/**
+ * @author ylyu
+ * @version 1.0
+ * @date 1/2/22 4:06 PM
+ */
+public interface DessertFactory {
+    Coffee createCoffee();
+    Dessert createDessert();
+}
