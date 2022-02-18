8924c78a2348c8257b53104299bdfa74e91b0194
Yanan Lyu <yanan.lyu@gmail.com>
2022-02-17 21:34:35 -0800

    Strategy

---
+++ OOD/src/main/java/design_pattern/strategy/Strategy.java
@@ -0,0 +1,11 @@
+package design_pattern.strategy;
+
+/**
+ * @author Yanan Lyu
+ * @date 2/16/22 10:06 AM
+ * @description 抽象策略（Strategy）类：是一个抽象角色，常由一个接口或抽象类实现。此角色给出所有
+ * 的具体策略类所有的接口。
+ */
+public interface Strategy {
+    void show();
+}
---
+++ OOD/src/main/java/design_pattern/strategy/StrategyC.java
@@ -0,0 +1,14 @@
+package design_pattern.strategy;
+
+/**
+ * @author Yanan Lyu
+ * @date 2/16/22 10:07 AM
+ * @description 具体策略(Concrete Strategy)类: 实现了抽象策略定义的接口,提供具体的算法实现或行
+ * 为。
+ */
+public class StrategyC implements Strategy {
+    @Override
+    public void show() {
+        System.out.println("满1000元加一元换购任意200元以下商");
+    }
+}
