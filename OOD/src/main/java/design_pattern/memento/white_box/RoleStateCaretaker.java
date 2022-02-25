22ee05048af59219085cd747466502d1d38ddd0e
Yanan Lyu <yanan.lyu@gmail.com>
2022-02-25 10:38:26 -0800

    memento

---
+++ OOD/src/main/java/design_pattern/memento/white_box/RoleStateCaretaker.java
@@ -0,0 +1,19 @@
+package design_pattern.memento.white_box;
+
+/**
+ * @author Yanan Lyu
+ * @date 2/25/22 9:55 AM
+ * @description 备忘录对象管理对象
+ */
+public class RoleStateCaretaker {
+    // 声明RoleStateMemento类型的变量
+    private RoleStateMemento roleStateMemento;
+
+    public RoleStateMemento getRoleStateMemento() {
+        return roleStateMemento;
+    }
+
+    public void setRoleStateMemento(RoleStateMemento roleStateMemento) {
+        this.roleStateMemento = roleStateMemento;
+    }
+}
