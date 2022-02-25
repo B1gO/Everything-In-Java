d71d17d1cc66fb8824668a55ab76af17a6bf35da
Yanan Lyu <yanan.lyu@gmail.com>
2022-02-24 22:00:46 -0800

    iterator

---
+++ OOD/src/main/java/design_pattern/iterator/StudentIterator.java
@@ -0,0 +1,11 @@
+package design_pattern.iterator;
+
+/**
+ * @author Yanan Lyu
+ * @date 2/24/22 9:40 PM
+ * @description 抽象迭代器角色接口
+ */
+public interface StudentIterator {
+    boolean hasNext();
+    Student next();
+}
---
+++ OOD/src/main/java/design_pattern/iterator/StudentIteratorImpl.java
@@ -0,0 +1,28 @@
+package design_pattern.iterator;
+
+import java.util.List;
+
+/**
+ * @author Yanan Lyu
+ * @date 2/24/22 9:41 PM
+ * @description 具体迭代器角色类
+ */
+public class StudentIteratorImpl implements StudentIterator {
+
+    private List<Student> list;
+    private int position = 0;   // 用来记录遍历时的位置
+
+    public StudentIteratorImpl(List<Student> list) {
+        this.list = list;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return position < list.size();
+    }
+
+    @Override
+    public Student next() {
+        return list.get(position++);
+    }
+}
